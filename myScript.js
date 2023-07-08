e2fd6c3d4370dc292893d869b1a2705649960671
coolpisces <coolpisces22@gmail.com>
2023-07-08 14:27:09 +0300

    finished

--- myScript.js
+++ myScript.js
@@ -2,24 +2,48 @@ const celsius = document.getElementById('celsius');
 const fahrenheit = document.getElementById('fahrenheit');
 const kelvin = document.getElementById('kelvin');
 
-function calculateTemp(event) {
-    debugger;
-    const currentValue = Number(event.target.value);
+const inputs = document.querySelectorAll('input');
 
-    switch (event.target.name) {
-        case 'celsius':
-            kelvin.value = (currentValue + 273.32).toFixed(2);
-            fahrenheit.value = (currentValue * 1.8 + 32).toFixed(2);
-            break;
-        case 'fahrenheit':
-            kelvin.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
-            celsius.value = ((currentValue - 32) * 1.8).toFixed(2);
-            break;
-        case 'kelvin':
-            celsius.value = (currentValue - 273.32).toFixed(2);
-            fahrenheit.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
-            break;
-        default:
-            break;
-    }
-}
+inputs.forEach(input => {
+    input.addEventListener('change', e => {
+        const currentValue = Number(e.target.value);
+        switch (e.target.name) {
+            case 'celsius':
+                kelvin.value = (currentValue + 273.32).toFixed(2);
+                fahrenheit.value = (currentValue * 1.8 + 32).toFixed(2);
+                break;
+            case 'fahrenheit':
+                kelvin.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+                celsius.value = ((currentValue - 32) * 1.8).toFixed(2);
+                break;
+            case 'kelvin':
+                celsius.value = (currentValue - 273.32).toFixed(2);
+                fahrenheit.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+                break;
+            default:
+                break;
+        }
+    })
+})
+
+// function calculateTemp(event) {
+
+//     const currentValue = Number(event.target.value);
+
+// switch (event.target.name) {
+//     case 'celsius':
+//         kelvin.value = (currentValue + 273.32).toFixed(2);
+//         fahrenheit.value = (currentValue * 1.8 + 32).toFixed(2);
+//         break;
+//     case 'fahrenheit':
+//         kelvin.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+//         celsius.value = ((currentValue - 32) * 1.8).toFixed(2);
+//         break;
+//     case 'kelvin':
+//         celsius.value = (currentValue - 273.32).toFixed(2);
+//         fahrenheit.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+//         break;
+//     default:
+//         break;
+// }
+// }
